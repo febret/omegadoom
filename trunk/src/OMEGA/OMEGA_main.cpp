2268d33eeefc7b4fef5ebada85a99b1ea33ec31c
febret@gmail.com <febret@gmail.com@2510cb4e-c5e7-ff21-45d9-53e416f016e0>
2011-11-14 21:42:51 +0000

    NEW: Initial import

---
+++ trunk/src/OMEGA/OMEGA_main.cpp
@@ -0,0 +1,347 @@
+/********************************************************************************************************************** 
+ * THE OMEGA LIB PROJECT
+ *---------------------------------------------------------------------------------------------------------------------
+ * Copyright 2010								Electronic Visualization Laboratory, University of Illinois at Chicago
+ * Authors:										
+ *  Alessandro Febretti							febret@gmail.com
+ *---------------------------------------------------------------------------------------------------------------------
+ * Copyright (c) 2010, Electronic Visualization Laboratory, University of Illinois at Chicago
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+ * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+ * and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *-----------------------------------------------------------------------------
+ *
+ *  PrBoom: a Doom port merged with LxDoom and LSDLDoom
+ *  based on BOOM, a modified and improved DOOM engine
+ *  Copyright 2005, 2006 by
+ *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
+ *
+ *********************************************************************************************************************/
+//#define OMEGA_NO_GL_HEADERS
+#include<omega.h>
+
+extern "C"
+{
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include "doomdef.h"
+#include "m_argv.h"
+#include "d_main.h"
+#include "m_fixed.h"
+#include "i_system.h"
+#include "i_video.h"
+#include "z_zone.h"
+#include "lprintf.h"
+#include "m_random.h"
+#include "doomstat.h"
+#include "g_game.h"
+#include "m_misc.h"
+#include "i_sound.h"
+#include "i_main.h"
+#include "r_fps.h"
+#include "lprintf.h"
+#include "m_menu.h"
+#include "s_sound.h"
+#include "d_net.h"
+#include "p_checksum.h"
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void I_Quit (void);
+void D_DoomMainSetup(void);
+void I_UpdateVideoMode(void);
+
+int OMEGA_channel_width;
+int OMEGA_channel_height;
+int OMEGA_canvas_width;
+int OMEGA_canvas_height;
+
+}
+
+using namespace omega;
+
+Lock sGlobalLock;
+
+extern const omega::DrawContext* sCurrentDrawContext;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+static int I_TranslateKey(int key)
+{
+  int rc = 0;
+
+  switch (key) 
+  {
+	  case KC_LEFT: rc = KEYD_LEFTARROW;  break;
+	  case KC_RIGHT:  rc = KEYD_RIGHTARROW; break;
+	  case KC_DOWN: rc = KEYD_DOWNARROW;  break;
+	  case KC_UP:   rc = KEYD_UPARROW;  break;
+	  case KC_ESCAPE: rc = KEYD_ESCAPE; break;
+	  case KC_RETURN: rc = KEYD_ENTER;  break;
+	  case KC_TAB:  rc = KEYD_TAB;    break;
+	  case KC_F1:   rc = KEYD_F1;   break;
+	  case KC_F2:   rc = KEYD_F2;   break;
+	  case KC_F3:   rc = KEYD_F3;   break;
+	  case KC_F4:   rc = KEYD_F4;   break;
+	  case KC_F5:   rc = KEYD_F5;   break;
+	  case KC_F6:   rc = KEYD_F6;   break;
+	  case KC_F7:   rc = KEYD_F7;   break;
+	  case KC_F8:   rc = KEYD_F8;   break;
+	  case KC_F9:   rc = KEYD_F9;   break;
+	  case KC_F10:  rc = KEYD_F10;    break;
+	  case KC_F11:  rc = KEYD_F11;    break;
+	  case KC_F12:  rc = KEYD_F12;    break;
+	  case KC_BACKSPACE:  rc = KEYD_BACKSPACE;  break;
+	  case KC_PAGE_UP: rc = KEYD_PAGEUP; break;
+	  case KC_PAGE_DOWN: rc = KEYD_PAGEDOWN; break;
+	  case KC_HOME: rc = KEYD_HOME; break;
+	  case KC_END:  rc = KEYD_END;  break;
+	  case KC_SHIFT_L:
+	  case KC_SHIFT_R: rc = KEYD_RSHIFT; break;
+	  case KC_CONTROL_L:
+	  case KC_CONTROL_R:  rc = KEYD_RCTRL;  break;
+	  case KC_ALT_L:
+	  case KC_ALT_R: rc = KEYD_RALT;   break;
+	  default:    rc = key;    break;
+  }
+  return rc;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class OmegaDoomClient: public ApplicationClient
+{
+public:
+	OmegaDoomClient(ApplicationServer* server): ApplicationClient(server) {}
+	virtual void initialize();
+	virtual void draw(const DrawContext& context);
+
+private:
+	bool myNeedGraphicsInit;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class OmegaDoomServer: public ApplicationServer
+{
+	enum Buttons {Button1, Button2, Button3, Button4, LSButton, RSButton, MaxButtons };
+public:
+	OmegaDoomServer(Application* app): ApplicationServer(app) {}
+	virtual void initialize();
+	virtual void handleEvent(const Event& evt);
+	virtual void update(const UpdateContext& context);
+
+private:
+	bool myButtonState[MaxButtons];
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class OmegaDoom: public Application
+{
+public:
+	virtual ApplicationServer* createServer() { return new OmegaDoomServer(this); }
+	virtual ApplicationClient* createClient(ApplicationServer* server) { return new OmegaDoomClient(server); }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void OmegaDoomServer::initialize()
+{
+	memset(myButtonState, 0, sizeof(bool) * MaxButtons);
+	D_DoomMainSetup(); // CPhipps - setup out of main execution stack
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void OmegaDoomServer::update(const UpdateContext& context)
+{
+	sGlobalLock.lock();
+    WasRenderedInTryRunTics = false;
+    // frame syncronous IO operations
+    I_StartFrame ();
+
+    if (ffmap == gamemap) ffmap = 0;
+
+    // process one or more tics
+    if (singletics)
+    {
+        I_StartTic ();
+        G_BuildTiccmd (&netcmds[consoleplayer][maketic%BACKUPTICS]);
+        if (advancedemo)
+        D_DoAdvanceDemo ();
+        M_Ticker ();
+        G_Ticker ();
+        P_Checksum(gametic);
+        gametic++;
+        maketic++;
+    }
+    else
+	{
+		TryRunTics (); // will run at least one tic
+	}
+
+    if (players[displayplayer].mo) S_UpdateSounds(players[displayplayer].mo);// move positional sounds
+	sGlobalLock.unlock();
+}
+
+#define SET_BUTTON_STATE(btn, index) if(evt.getExtraDataFloat(index) > 0) { myButtonState[btn] = true; evt.setProcessed(); } else myButtonState[btn] = false;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void OmegaDoomServer::handleEvent(const Event& evt)
+{
+	event_t event;
+	if(evt.getServiceType() == Service::Keyboard)
+	{
+		int keycode = I_TranslateKey(evt.getSourceId());
+		if(evt.getType() == Event::Down)
+		{
+			event.type = ev_keydown;
+			event.data1 = keycode;
+			D_PostEvent(&event);
+		}
+		else if(evt.getType() == Event::Up)
+		{
+			event.type = ev_keyup;
+			event.data1 = keycode;
+			D_PostEvent(&event);
+		}
+	}
+	else if(evt.getServiceType() == Service::Controller)
+	{
+		float n = 1000;
+
+		float x = evt.getExtraDataFloat(1) / n;
+		float y = evt.getExtraDataFloat(2) / n;
+		float z = evt.getExtraDataFloat(5) / n;
+		float yaw = evt.getExtraDataFloat(3) / n;
+		float pitch = evt.getExtraDataFloat(4) / n;
+		float trigger = evt.getExtraDataFloat(5) / n;
+		float tresh = 0.2f;
+
+		if(x < tresh && x > -tresh) x = 0;
+		if(y < tresh && y > -tresh) y = 0;
+		if(z < tresh && z > -tresh) z = 0;
+		if(yaw < tresh && yaw > -tresh) yaw = 0;
+		if(pitch < tresh && pitch > -tresh) pitch = 0;
+		if(trigger < tresh && trigger > -tresh) trigger = 0;
+
+	    event.type = ev_joystick;
+		int xaxis = (x * 32000);
+		int yaxis = (y * 32000);
+
+		event.data2 = xaxis;
+		event.data3 = yaxis;
+
+		SET_BUTTON_STATE(Button1, 6);
+		SET_BUTTON_STATE(Button2, 7);
+		SET_BUTTON_STATE(Button3, 8);
+		SET_BUTTON_STATE(Button4, 9);
+		SET_BUTTON_STATE(RSButton, 10);
+		SET_BUTTON_STATE(RSButton, 11);
+
+		// Trigger used as button 1.
+		if(trigger < 0) event.data1 = 1;
+
+		event.data1 |= (
+			(myButtonState[Button1] << 1) |
+			(myButtonState[Button2] << 2) |
+			(myButtonState[Button3] << 3));
+
+		D_PostEvent(&event);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void OmegaDoomClient::initialize()
+{
+	myNeedGraphicsInit = true;
+	/* Version info */
+	//lprintf(LO_INFO,"\n");
+	//PrintVer();
+
+	/* cph - Z_Close must be done after I_Quit, so we register it first. */
+	atexit(Z_Close);
+
+	Z_Init();                  /* 1/18/98 killough: start up memory stuff first */
+
+	atexit(I_Quit);
+
+#ifndef _DEBUG
+	signal(SIGSEGV, I_SignalHandler);
+	signal(SIGTERM, I_SignalHandler);
+	signal(SIGFPE,  I_SignalHandler);
+	signal(SIGILL,  I_SignalHandler);
+	signal(SIGINT,  I_SignalHandler);  /* killough 3/6/98: allow CTRL-BRK during init */
+	signal(SIGABRT, I_SignalHandler);
+#endif
+
+	I_SetAffinityMask();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void OmegaDoomClient::draw(const DrawContext& context)
+{
+	sGlobalLock.lock();
+
+	if(context.task == DrawContext::SceneDrawTask)
+	{
+		sCurrentDrawContext = &context;
+
+		if(myNeedGraphicsInit)
+		{
+			myNeedGraphicsInit = false;
+
+			OMEGA_channel_width = context.viewport.width();
+			OMEGA_channel_height  = context.viewport.height();
+			OMEGA_canvas_width = context.channel->canvasSize->x();
+			OMEGA_canvas_height = context.channel->canvasSize->y();
+
+			I_PreInitGraphics();
+			I_UpdateVideoMode();
+		}
+
+		SCREENWIDTH = context.channel->canvasSize->x();
+		SCREENHEIGHT = context.channel->canvasSize->x();
+
+		if (V_GetMode() == VID_MODEGL ? 
+		!movement_smooth || !WasRenderedInTryRunTics :
+		!movement_smooth || !WasRenderedInTryRunTics || gamestate != wipegamestate)
+		{
+			glDisable(GL_LIGHTING);
+			// Update display, next frame, with current state.
+			D_Display();
+		}
+	}
+
+	sGlobalLock.unlock();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+	myargc = argc;
+	myargv = (const char * const *) argv;
+
+	OmegaDoom app;
+
+	// Read config file name from command line or use default one.
+	// NOTE: being a simple application, ohello does not have any application-specific configuration option. 
+	// So, we are going to load directly a system configuration file.
+	const char* cfgName = "system/desktop.cfg";
+	if(argc == 2) cfgName = argv[1];
+
+	omain(app, cfgName, "omegadoom.log", new FilesystemDataSource(OMEGA_DATA_PATH));
+}
---
+++ trunk/src/OMEGA/i_video.c
@@ -0,0 +1,478 @@
+/********************************************************************************************************************** 
+ * THE OMEGA LIB PROJECT
+ *---------------------------------------------------------------------------------------------------------------------
+ * Copyright 2010								Electronic Visualization Laboratory, University of Illinois at Chicago
+ * Authors:										
+ *  Alessandro Febretti							febret@gmail.com
+ *---------------------------------------------------------------------------------------------------------------------
+ * Copyright (c) 2010, Electronic Visualization Laboratory, University of Illinois at Chicago
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+ * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
+ * and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *-----------------------------------------------------------------------------
+ *
+ *  PrBoom: a Doom port merged with LxDoom and LSDLDoom
+ *  based on BOOM, a modified and improved DOOM engine
+ *  Copyright 2005, 2006 by
+ *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
+ *
+ *********************************************************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#include "SDL.h"
+
+#include "m_argv.h"
+#include "doomstat.h"
+#include "doomdef.h"
+#include "doomtype.h"
+#include "v_video.h"
+#include "r_draw.h"
+#include "d_main.h"
+#include "d_event.h"
+#include "i_joy.h"
+#include "i_video.h"
+#include "z_zone.h"
+#include "s_sound.h"
+#include "sounds.h"
+#include "w_wad.h"
+#include "st_stuff.h"
+#include "lprintf.h"
+#include "i_pngshot.h"
+
+#include "OMEGA/OMEGA_interface.h"
+
+int gl_colorbuffer_bits=16;
+int gl_depthbuffer_bits=16;
+
+extern void M_QuitDOOM(int choice);
+#ifdef DISABLE_DOUBLEBUFFER
+int use_doublebuffer = 0;
+#else
+int use_doublebuffer = 1; // Included not to break m_misc, but not relevant to SDL
+#endif
+int use_fullscreen;
+int desired_fullscreen;
+static SDL_Surface *screen;
+
+////////////////////////////////////////////////////////////////////////////
+// Input code
+int             leds_always_off = 0; // Expected by m_misc, not relevant
+
+// Mouse handling
+extern int     usemouse;        // config file var
+static boolean mouse_enabled; // usemouse, but can be overriden by -nomouse
+static boolean mouse_currently_grabbed;
+
+/////////////////////////////////////////////////////////////////////////////////
+// Main input code
+
+/* cph - pulled out common button code logic */
+static int I_SDLtoDoomMouseState(Uint8 buttonstate)
+{
+  return 0
+      | (buttonstate & SDL_BUTTON(1) ? 1 : 0)
+      | (buttonstate & SDL_BUTTON(2) ? 2 : 0)
+      | (buttonstate & SDL_BUTTON(3) ? 4 : 0);
+}
+
+static void I_GetEvent(SDL_Event *Event)
+{
+  event_t event;
+
+  switch (Event->type) {
+  case SDL_MOUSEBUTTONDOWN:
+  case SDL_MOUSEBUTTONUP:
+  if (mouse_enabled) // recognise clicks even if the pointer isn't grabbed
+  {
+    event.type = ev_mouse;
+    event.data1 = I_SDLtoDoomMouseState(SDL_GetMouseState(NULL, NULL));
+    event.data2 = event.data3 = 0;
+    D_PostEvent(&event);
+  }
+  break;
+
+  case SDL_MOUSEMOTION:
+  if (mouse_currently_grabbed) {
+    event.type = ev_mouse;
+    event.data1 = I_SDLtoDoomMouseState(Event->motion.state);
+    event.data2 = Event->motion.xrel << 5;
+    event.data3 = -Event->motion.yrel << 5;
+    D_PostEvent(&event);
+  }
+  break;
+
+
+  case SDL_QUIT:
+    S_StartSound(NULL, sfx_swtchn);
+    M_QuitDOOM(0);
+
+  default:
+    break;
+  }
+}
+
+
+//
+// I_StartTic
+//
+
+void I_StartTic (void)
+{
+  SDL_Event Event;
+  {
+    boolean should_be_grabbed = mouse_enabled &&
+      !(paused || (gamestate != GS_LEVEL) || demoplayback || menuactive);
+
+    if (mouse_currently_grabbed != should_be_grabbed)
+      SDL_WM_GrabInput((mouse_currently_grabbed = should_be_grabbed)
+          ? SDL_GRAB_ON : SDL_GRAB_OFF);
+  }
+
+  while ( SDL_PollEvent(&Event) )
+    I_GetEvent(&Event);
+
+  I_PollJoystick();
+}
+
+//
+// I_StartFrame
+//
+void I_StartFrame (void)
+{
+}
+
+//
+// I_InitInputs
+//
+
+static void I_InitInputs(void)
+{
+  int nomouse_parm = M_CheckParm("-nomouse");
+
+  // check if the user wants to use the mouse
+  mouse_enabled = usemouse && !nomouse_parm;
+
+  // e6y: fix for turn-snapping bug on fullscreen in software mode
+  if (!nomouse_parm)
+    SDL_WarpMouse((unsigned short)(SCREENWIDTH/2), (unsigned short)(SCREENHEIGHT/2));
+
+  I_InitJoystick();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Graphics API
+
+void I_ShutdownGraphics(void)
+{
+}
+
+//
+// I_UpdateNoBlit
+//
+void I_UpdateNoBlit (void)
+{
+}
+
+//
+// I_FinishUpdate
+//
+void I_FinishUpdate (void)
+{
+  if (V_GetMode() == VID_MODEGL) 
+  {
+    gld_Finish();
+  }
+}
+
+//
+// I_ScreenShot
+//
+
+int I_ScreenShot (const char *fname)
+{
+  //if (V_GetMode() == VID_MODEGL)
+  //{
+  //  int result = -1;
+  //  unsigned char *pixel_data = gld_ReadScreen();
+
+  //  if (pixel_data)
+  //  {
+  //    SDL_Surface *surface = SDL_CreateRGBSurfaceFrom(
+  //        pixel_data, SCREENWIDTH, SCREENHEIGHT, 24, SCREENWIDTH*3,
+  //        0x000000ff, 0x0000ff00, 0x00ff0000, 0);
+
+  //    if (surface)
+  //    {
+  //      result = SAVE_PNG_OR_BMP(surface, fname);
+  //      SDL_FreeSurface(surface);
+  //    }
+  //    free(pixel_data);
+  //  }
+  //  return result;
+  //}
+}
+
+//
+// I_SetPalette
+//
+void I_SetPalette (int pal)
+{
+}
+
+// I_PreInitGraphics
+
+static void I_ShutdownSDL(void)
+{
+  SDL_Quit();
+  return;
+}
+
+void I_PreInitGraphics(void)
+{
+  static const union {
+    const char *c;
+    char *s;
+  } window_pos = {"SDL_VIDEO_WINDOW_POS=center"};
+
+  unsigned int flags = 0;
+
+  putenv(window_pos.s);
+
+  // Initialize SDL
+  if (!(M_CheckParm("-nodraw") && M_CheckParm("-nosound")))
+    flags = SDL_INIT_VIDEO;
+#ifdef _DEBUG
+  flags |= SDL_INIT_NOPARACHUTE;
+#endif
+  if ( SDL_Init(flags) < 0 ) {
+    I_Error("Could not initialize SDL [%s]", SDL_GetError());
+  }
+
+  atexit(I_ShutdownSDL);
+}
+
+// CPhipps -
+// I_CalculateRes
+// Calculates the screen resolution, possibly using the supplied guide
+void I_CalculateRes(unsigned int width, unsigned int height)
+{
+	SCREENHEIGHT = OMEGA_canvas_height;
+	SCREENWIDTH = OMEGA_canvas_width;
+}
+
+// CPhipps -
+// I_SetRes
+// Sets the screen resolution
+void I_SetRes(void)
+{
+  int i;
+
+  I_CalculateRes(SCREENWIDTH, SCREENHEIGHT);
+
+  // set first three to standard values
+  for (i=0; i<3; i++) {
+    screens[i].width = SCREENWIDTH;
+    screens[i].height = SCREENHEIGHT;
+    screens[i].byte_pitch = SCREENPITCH;
+    screens[i].short_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE16);
+    screens[i].int_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE32);
+  }
+
+  // statusbar
+  screens[4].width = SCREENWIDTH;
+  screens[4].height = (ST_SCALED_HEIGHT+1);
+  screens[4].byte_pitch = SCREENPITCH;
+  screens[4].short_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE16);
+  screens[4].int_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE32);
+
+  lprintf(LO_INFO,"I_SetRes: Using resolution %dx%d\n", SCREENWIDTH, SCREENHEIGHT);
+}
+
+void I_InitGraphics(void)
+{
+  //char titlebuffer[2048];
+  static int    firsttime=1;
+
+  if (firsttime)
+  {
+    firsttime = 0;
+
+    //atexit(I_ShutdownGraphics);
+    //lprintf(LO_INFO, "I_InitGraphics: %dx%d\n", SCREENWIDTH, SCREENHEIGHT);
+
+    ///* Set the video mode */
+    //I_UpdateVideoMode();
+
+    ///* Setup the window title */
+    //strcpy(titlebuffer,PACKAGE);
+    //strcat(titlebuffer," ");
+    //strcat(titlebuffer,VERSION);
+    //SDL_WM_SetCaption(titlebuffer, titlebuffer);
+
+    /* Initialize the input system */
+    I_InitInputs();
+  }
+}
+
+int I_GetModeFromString(const char *modestr)
+{
+  video_mode_t mode;
+
+  if (!stricmp(modestr,"15")) {
+    mode = VID_MODE15;
+  } else if (!stricmp(modestr,"15bit")) {
+    mode = VID_MODE15;
+  } else if (!stricmp(modestr,"16")) {
+    mode = VID_MODE16;
+  } else if (!stricmp(modestr,"16bit")) {
+    mode = VID_MODE16;
+  } else if (!stricmp(modestr,"32")) {
+    mode = VID_MODE32;
+  } else if (!stricmp(modestr,"32bit")) {
+    mode = VID_MODE32;
+  } else if (!stricmp(modestr,"gl")) {
+    mode = VID_MODEGL;
+  } else if (!stricmp(modestr,"OpenGL")) {
+    mode = VID_MODEGL;
+  } else {
+    mode = VID_MODE8;
+  }
+  return mode;
+}
+
+void I_UpdateVideoMode(void)
+{
+  //int init_flags;
+  int i;
+  video_mode_t mode;
+
+  lprintf(LO_INFO, "I_UpdateVideoMode: %dx%d (%s)\n", SCREENWIDTH, SCREENHEIGHT, desired_fullscreen ? "fullscreen" : "nofullscreen");
+
+  mode = I_GetModeFromString(default_videomode);
+  if ((i=M_CheckParm("-vidmode")) && i<myargc-1) {
+    mode = I_GetModeFromString(myargv[i+1]);
+  }
+
+  V_InitMode(mode);
+  V_DestroyUnusedTrueColorPalettes();
+  V_FreeScreens();
+
+  I_SetRes();
+
+#if 0
+  // Initialize SDL with this graphics mode
+  if (V_GetMode() == VID_MODEGL) {
+    init_flags = SDL_OPENGL;
+  } else {
+    if (use_doublebuffer)
+      init_flags = SDL_DOUBLEBUF;
+    else
+      init_flags = SDL_SWSURFACE;
+#ifndef _DEBUG
+    init_flags |= SDL_HWPALETTE;
+#endif
+  }
+
+  if ( desired_fullscreen )
+    init_flags |= SDL_FULLSCREEN;
+
+  if (V_GetMode() == VID_MODEGL) {
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_ACCUM_ALPHA_SIZE, 0 );
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_SetAttribute( SDL_GL_BUFFER_SIZE, gl_colorbuffer_bits );
+    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, gl_depthbuffer_bits );
+    screen = SDL_SetVideoMode(SCREENWIDTH, SCREENHEIGHT, gl_colorbuffer_bits, init_flags);
+  } else {
+    screen = SDL_SetVideoMode(SCREENWIDTH, SCREENHEIGHT, V_GetNumPixelBits(), init_flags);
+  }
+
+  if(screen == NULL) {
+    I_Error("Couldn't set %dx%d video mode [%s]", SCREENWIDTH, SCREENHEIGHT, SDL_GetError());
+  }
+
+  lprintf(LO_INFO, "I_UpdateVideoMode: 0x%x, %s, %s\n", init_flags, screen->pixels ? "SDL buffer" : "own buffer", SDL_MUSTLOCK(screen) ? "lock-and-copy": "direct access");
+
+  mouse_currently_grabbed = false;
+
+  // Get the info needed to render to the display
+  if (!SDL_MUSTLOCK(screen))
+  {
+    screens[0].not_on_heap = true;
+    screens[0].data = (unsigned char *) (screen->pixels);
+    screens[0].byte_pitch = screen->pitch;
+    screens[0].short_pitch = screen->pitch / V_GetModePixelDepth(VID_MODE16);
+    screens[0].int_pitch = screen->pitch / V_GetModePixelDepth(VID_MODE32);
+  }
+  else
+  {
+    screens[0].not_on_heap = false;
+  }
+#endif
+
+	screens[0].not_on_heap = false;
+
+  V_AllocScreens();
+
+  // Hide pointer while over this window
+  //SDL_ShowCursor(0);
+
+  R_InitBuffer(SCREENWIDTH, SCREENHEIGHT);
+
+  if (V_GetMode() == VID_MODEGL) {
+    int temp;
+    lprintf(LO_INFO,"SDL OpenGL PixelFormat:\n");
+    SDL_GL_GetAttribute( SDL_GL_RED_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_RED_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_GREEN_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_GREEN_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_BLUE_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_BLUE_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_STENCIL_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_STENCIL_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_ACCUM_RED_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_ACCUM_RED_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_ACCUM_GREEN_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_ACCUM_GREEN_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_ACCUM_BLUE_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_ACCUM_BLUE_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_ACCUM_ALPHA_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_ACCUM_ALPHA_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_DOUBLEBUFFER, &temp );
+    lprintf(LO_INFO,"    SDL_GL_DOUBLEBUFFER: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_BUFFER_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_BUFFER_SIZE: %i\n",temp);
+    SDL_GL_GetAttribute( SDL_GL_DEPTH_SIZE, &temp );
+    lprintf(LO_INFO,"    SDL_GL_DEPTH_SIZE: %i\n",temp);
+#ifdef GL_DOOM
+    gld_Init(SCREENWIDTH, SCREENHEIGHT);
+#endif
+  }
+}
